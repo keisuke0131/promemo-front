6b5d23179c10f3f23179c405e3aeb0a94c61c7d3
SHIRO-11 <howaito01@gmail.com>
2021-06-21 11:30:12 +0900

    add： Vue.Draggableをインストールしました。

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -3,11 +3,17 @@
     <main>
       <Sidebar page="2" />
       <div class="main-contents">
-        <div class="category" v-for="category of categories" :key="category.id">
-          <div>
-            <h3>{{ category.name }}</h3>
-            <div class="memo" v-for="post in category.posts" :key="post.id">
-              {{ post.title }}
+        <div class="category-scroll">
+          <div
+            class="category"
+            v-for="category of categories"
+            :key="category.id"
+          >
+            <div>
+              <h3>{{ category.name }}</h3>
+              <div class="memo" v-for="post in category.posts" :key="post.id">
+                {{ post.title }}
+              </div>
             </div>
           </div>
         </div>
@@ -38,22 +44,25 @@ main {
   width: 1250px;
   margin: 50px auto 0 auto;
   .main-contents {
-    display: flex;
-    flex-wrap: wrap;
     width: 1000px;
-    .category {
-      width: calc(35% - 11.25px);
-      margin: 0 15px 15px 0;
-      padding: 20px;
-      box-shadow: 0 5px 10px -3px rgba(0, 0, 0, 0.3);
-      border: 1px solid $BORDER_GRAY01;
-      &:nth-of-type(4n) {
-        margin: 0 0 15px 0;
-      }
+  }
 
-      h3 {
-        margin-bottom: 20px;
-      }
+  .category-scroll {
+    width: 100%;
+    overflow: scroll;
+    display: flex;
+  }
+
+  .category {
+    width: 300px;
+    min-width: 300px;
+    margin: 0 15px 15px 0;
+    padding: 20px;
+    box-shadow: 0 5px 10px -3px rgba(0, 0, 0, 0.3);
+    border: 1px solid $BORDER_GRAY01;
+
+    h3 {
+      margin-bottom: 20px;
     }
 
     .memo {
@@ -61,9 +70,6 @@ main {
       margin: 0 15px 15px 0;
       padding: 20px;
       border: 1px solid $BORDER_GRAY01;
-      &:nth-of-type(4n) {
-        margin: 0 0 15px 0;
-      }
     }
   }
 }
