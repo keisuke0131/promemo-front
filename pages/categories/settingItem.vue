e28c7d4abef4637a3233ba22745dc7db67126e3c
SHIRO-11 <howaito01@gmail.com>
2021-06-24 12:11:27 +0900

    update：カテゴリーの色を増やしました。

--- pages/categories/settingItem.vue
+++ pages/categories/settingItem.vue
@@ -18,27 +18,116 @@
         ></div>
       </template>
 
-      <div class="color-pallet" v-if="isOpenPallet">
-        <span
-          @click="changeCategorColor($event, categoryProp.id)"
-          class="red1"
-        ></span>
-        <span
-          @click="changeCategorColor($event, categoryProp.id)"
-          class="orange1"
-        ></span>
-        <span
-          @click="changeCategorColor($event, categoryProp.id)"
-          class="orange2"
-        ></span>
-        <span
-          @click="changeCategorColor($event, categoryProp.id)"
-          class="blue1"
-        ></span>
-        <span
-          @click="changeCategorColor($event, categoryProp.id)"
-          class="yellow1"
-        ></span>
+      <div v-if="isOpenPallet">
+        <div @click="togglePallet" class="pallet-bg"></div>
+        <div class="color-pallet">
+          <div class="pallet-content">
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="red1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="red2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="red3"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="pink1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="pink2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="pink3"
+            ></span>
+
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="purple1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="purple2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="purple3"
+            ></span>
+
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="blue1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="blue2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="blue3"
+            ></span>
+
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="skyBlue1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="skyBlue2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="skyBlue3"
+            ></span>
+
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="green1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="green2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="green3"
+            ></span>
+
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="orange1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="orange2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="orange3"
+            ></span>
+
+
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="yellow1"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="yellow2"
+            ></span>
+            <span
+              @click="changeCategorColor($event, categoryProp.id)"
+              class="yellow3"
+            ></span>
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -89,16 +178,34 @@ export default {
     position: absolute;
     top: 20px;
     right: -20px;
-    width: 200px;
-    height: 200px;
-    display: flex;
+    width: 250px;
+    height: 250px;
+    z-index: 9999;
 
     span {
-      width: 20px;
-      height: 20px;
+      width: 25px;
+      height: 25px;
       display: block;
       cursor: pointer;
     }
+  }
+
+  .pallet-content {
+    display: flex;
+    flex-wrap: wrap;
+    background: white;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+      rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+    z-index: 9999;
+  }
+
+  .pallet-bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 99;
   }
 }
 </style>
