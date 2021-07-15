8ee5c5924c2303a2b98e3a2b3ce02382e3ba52ab
SHIRO-11 <howaito01@gmail.com>
2021-07-15 15:03:30 +0900

    update：修正

--- components/LikeComponent.vue
+++ components/LikeComponent.vue
@@ -15,13 +15,9 @@ export default {
    }
  },
  created() {
-<<<<<<< Updated upstream
   //  this.first_check()
-=======
-  // this.first_check(); 
  },
  mounted() {
->>>>>>> Stashed changes
  },
  methods: {
    first_check() {
