afc0b04f3f9f677f4b0641731ce22be492fd90ae
SHIRO-11 <howaito01@gmail.com>
2022-03-14 17:09:08 +0900

    update：ログイン画面の背景変更

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
     <main>
+      <video id="video" webkit-playsinline playsinline muted autoplay loop>
+        <!--
+poster：動画ファイルが利用できない環境で代替表示される画像
+webkit-playsinline：iOS 9までのSafari用インライン再生指定
+playsinline：iOS 10以降のSafari用インライン再生指定
+muted：音声をミュートさせる
+autoplay：動画を自動再生させる
+loop：動画をループさせる
+controls：コントロールバーを表示する
+-->
+        <source src="@/assets/image/back-video.mp4" type="video/mp4" />
+        <p>動画を再生できる環境ではありません。</p>
+      </video>
       <div class="login-form-wrap">
         <div class="head">
-          <img src="@/assets/image/logo.png" alt="">
+          <img src="@/assets/image/logo.png" alt="" />
         </div>
         <form @submit.prevent="login">
           <div class="form-group">
-            <input type="text" id="email" v-model="user.email" placeholder="メールアドレス"/>
+            <input
+              type="text"
+              id="email"
+              v-model="user.email"
+              placeholder="メールアドレス"
+            />
           </div>
           <div class="form-group">
-            <input type="password" id="password" v-model="user.password" placeholder="パスワード" />
+            <input
+              type="password"
+              id="password"
+              v-model="user.password"
+              placeholder="パスワード"
+            />
           </div>
           <button type="submit">ログイン</button>
         </form>
@@ -50,27 +73,33 @@ export default {
           .catch((err) => {
             console.log(err);
           });
-      this.toggleLoading(true);
-      setTimeout(() => {
-        this.toggleLoading(false)
-      }, 3000)
+        this.toggleLoading(true);
+        setTimeout(() => {
+          this.toggleLoading(false);
+        }, 3000);
       });
     },
-    toggleLoading (loading) {
-      this.$nuxt.$emit('toggleLoading', loading)
-    }
+    toggleLoading(loading) {
+      this.$nuxt.$emit("toggleLoading", loading);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-main{
-  display:flex;
-  align-items:center;
+main {
+  display: flex;
+  align-items: center;
   position: relative;
   z-index: 2;
-  height:100vh;
-  background:url("@/assets/image/login-bg.jpg");
+  height: 100vh;
+  background: url("@/assets/image/login-bg.jpg");
+}
+
+video{
+  position: fixed;
+  height: 100%;
+  z-index: -1;
 }
 
 .login-form-wrap {
@@ -83,11 +112,11 @@ main{
   border-radius: 15px;
   background: rgba(255, 255, 255, 0.9);
   text-align: center;
-  .head{
+  .head {
     text-align: center;
-    img{
+    img {
       display: inline-block;
-      margin:0 0 30px 0;
+      margin: 0 0 30px 0;
     }
   }
   .form-group {
@@ -99,7 +128,7 @@ main{
     input {
       display: block;
       width: 100%;
-      margin:0 0 15px 0;
+      margin: 0 0 15px 0;
       padding: 10px 5px;
       border-bottom: 1px solid rgb(218, 218, 218);
       &:focus {
@@ -109,18 +138,21 @@ main{
   }
   button {
     display: inline-block;
-    margin:20px 0 0 0;
+    margin: 20px 0 0 0;
     padding: 10px 50px;
     border-radius: 3px;
-    background:rgb(78, 137, 219);
+    background: rgb(78, 137, 219);
     color: white;
     font-weight: bold;
   }
-  a,a:link,a:visited,a:active{
+  a,
+  a:link,
+  a:visited,
+  a:active {
     display: inline-block;
     margin: 20px 0 0 0;
-    color:#333;
-    font-size:0.7em;
+    color: #333;
+    font-size: 0.7em;
     text-decoration: underline;
   }
 }
